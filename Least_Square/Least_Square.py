70b38f83ee5a4f22ce819cec2320bdb2fe898b35
miladmz <mmoazezian@gmail.com>
2022-12-06 13:28:29 +0330

    least_square_update2

--- Least_Square/Least_Square.py
+++ Least_Square/Least_Square.py
@@ -4,9 +4,8 @@ import pandas as pd
 from osgeo import gdal
 import matplotlib.pyplot as plt
 import sys
-if not sys.warnoptions:
-    import warnings
-    warnings.simplefilter("ignore")
+import warnings
+warnings.filterwarnings("ignore")
 filenames = glob.glob('/media/miladmoazezian/3B07B9F56DF9B08D/Project_subsidence/Interferograms/*IW2.tif')
 time1 = []
 time2 = []
@@ -27,6 +26,7 @@ W = np.zeros([len(filenames) , len(filenames)])
 Desighn_Matrix = pd.DataFrame(A , columns=Unique_name)
 Unique_name = Unique_Time.astype(str)
 DELTA_PHASE = np.zeros([len(filenames) , 1])
+Phase = np.zeros([len(Unique_Time), np.shape(test_data)[0] , np.shape(test_data)[1]])
 
 for row in range(np.shape(test_data)[0]):
     for col in range(np.shape(test_data)[1]):
@@ -34,7 +34,10 @@ for row in range(np.shape(test_data)[0]):
             break
         x_geo = GT_test[1] * col + GT_test[2] * row + GT_test[1] * 0.5 + GT_test[2] * 0.5 + GT_test[0]
         y_geo = GT_test[4] * col + GT_test[5] * row + GT_test[4] * 0.5 + GT_test[5] * 0.5 + GT_test[3]
+
+
         for j in range(len(filenames)):
+            print(j)
             ds = gdal.Open(filenames[j])
             GT = ds.GetGeoTransform()
             c, a, b, f, d, e = GT
@@ -48,14 +51,16 @@ for row in range(np.shape(test_data)[0]):
             ID2 = np.where(Desighn_Matrix.columns == second_time)
             Desighn_Matrix.iloc[j , ID1] = 1
             Desighn_Matrix.iloc[j , ID2] = -1
+        print(row , col)
+        Desighn_Matrix_array = np.asarray(Desighn_Matrix)
+        Phase[: , row , col] = np.reshape(np.matmul(np.matmul(np.matmul(np.linalg.inv(np.matmul(np.matmul(Desighn_Matrix_array.T, W), Desighn_Matrix_array)), Desighn_Matrix_array.T),W), DELTA_PHASE) , [len(Unique_Time) ,])
 
 
 
-print(Desighn_Matrix.iloc[0,0])
-Desighn_Matrix = np.asarray(Desighn_Matrix)
-Phase = np.matmul(np.matmul(np.matmul(np.linalg.inv(np.matmul(np.matmul(Desighn_Matrix.T , W) , Desighn_Matrix)) , Desighn_Matrix.T) , W) , DELTA_PHASE)
 
-plt.plot(Phase)
+
+
+#plt.imshow(Phase)
 
 
 
